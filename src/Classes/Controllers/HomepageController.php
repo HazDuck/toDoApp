61c327bbbbd677529bdf9a5c16a230c8a4a88cba
HazDuck <peter.e.heyes@gmail.com>
2019-05-09 10:57:38 +0100

    created homepage controller and updated routes

---
+++ src/Classes/Controllers/HomepageController.php
@@ -0,0 +1,17 @@
+<?php
+namespace Portal\Controllers;
+
+use Slim\Views\PhpRenderer;
+
+class HomepageController {
+
+    public function $renderer;
+    public function $listModel;
+
+    public function __construct(PhpRenderer $renderer, )
+    {
+    }
+
+
+
+}
--- src/routes.php
+++ src/routes.php
@@ -4,14 +4,24 @@ use Slim\App;
 use Slim\Http\Request;
 use Slim\Http\Response;
 
+//return function (App $app) {
+//    $container = $app->getContainer();
+//
+//    $app->get('/[{name}]', function (Request $request, Response $response, array $args) use ($container) {
+//        // Sample log message
+//        $container->get('logger')->info("Slim-Skeleton '/' route");
+//
+//        // Render index view
+//        return $container->get('renderer')->render($response, 'index.phtml', $args);
+//    });
+//
+//};
+
 return function (App $app) {
-    $container = $app->getContainer();
+//    $container = $app->getContainer();
 
-    $app->get('/[{name}]', function (Request $request, Response $response, array $args) use ($container) {
-        // Sample log message
-        $container->get('logger')->info("Slim-Skeleton '/' route");
+    $app->get('/', //TODO ADD CONTROLLER HERE) {
 
-        // Render index view
-        return $container->get('renderer')->render($response, 'index.phtml', $args);
     });
+
 };
