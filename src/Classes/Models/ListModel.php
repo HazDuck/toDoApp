bb99a2cb690a6608df701b5322ca16a3c50aa5bb
HazDuck <peter.e.heyes@gmail.com>
2019-05-10 15:45:02 +0100

    completed adding new lists

--- src/Classes/Models/ListModel.php
+++ src/Classes/Models/ListModel.php
@@ -12,7 +12,7 @@ class ListModel {
     }
 
     public function getTasks () {
-        $query = $this->db->prepare("SELECT `task_table`.`id`,`task_table`.`task`, `to_do_lists_table`.`to_do_list` FROM `task_table` 
+        $query = $this->db->prepare("SELECT `task_table`.`id`,`task_table`.`task`, `task_table`.`to_do_list_id` , `to_do_lists_table`.`to_do_list` FROM `task_table` 
 LEFT JOIN `to_do_lists_table` 
     ON `task_table`.`to_do_list_id` = `to_do_lists_table`.`id` 
     WHERE `completed` = 0;");
